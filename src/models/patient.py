e636204011510321adb8f0492f59e76c131b3fc7
SeragAmged <seragamged2002@gmail.com>
2023-12-18 01:57:48 +0200

    init for models

--- src/models/patient.py
+++ src/models/patient.py
@@ -1,8 +1,26 @@
 from person import Person
-from  datetime import datetime
+from datetime import datetime
+
+
 class Patient(Person):
     gender: int
     date_of_birth: datetime
     home_address: str
     balance: float
-    number_of_records:int
+    number_of_records: int
+
+    def __init__(self, first_name: str, last_name: str, phone: str,
+                 gender: int, date_of_birth: datetime, home_address: str,
+                 balance: float, number_of_records: int) -> None:
+        super().__init__(first_name, last_name, phone)
+        self.gender = gender
+        self.date_of_birth = date_of_birth
+        self.home_address = home_address
+        self.balance = balance
+        self.number_of_records = number_of_records
+
+    def display_info(self):
+        super().display_info()
+        print(f"Gender: {self.gender}, Date of Birth: {self.date_of_birth.strftime('%Y-%m-%d')},"
+              f" Home Address: {self.home_address}, Balance: {self.balance},"
+              f" Number of Records: {self.number_of_records}")
--- src/models/room_type.py
+++ src/models/room_type.py
@@ -1,5 +1,9 @@
 class RoomType:
     type_id: int
     type_description: str
-    def __init__(self,type_description):
+
+    def __init__(self, type_description):
         self.type_description = type_description
+
+    def display_info(self):
+        print(f"Description: {self.type_description}")
