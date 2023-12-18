8ab4cb231c98b3db0f34db4e81f14626efa596e1
SeragAmged <seragamged2002@gmail.com>
2023-12-18 02:29:12 +0200

    id

--- src/models/doctor.py
+++ src/models/doctor.py
@@ -9,11 +9,14 @@ class Doctor(Person):
 
     def __init__(self, person_id: int, first_name: str, last_name: str, phone: str,
                  job_title: str, salary: int, email: str, office_number: int) -> None:
-        
+
         super().__init__(person_id, first_name, last_name, phone)
         self.job_title = job_title
         self.salary = salary
         self.email = email
         self.office_number = office_number
 
-
+    def display_info(self):
+        super().display_info()
+        print(
+            f"Job Title: {self.job_title}, Salary: {self.salary}, Email: {self.email}, Office Number: {self.office_number}")
