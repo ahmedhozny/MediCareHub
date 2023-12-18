4a3e8cded2ea397b705dd43aa78967060515d732
SeragAmged <seragamged2002@gmail.com>
2023-12-18 02:30:36 +0200

    word bout init display info

--- src/models/ward_boy.py
+++ src/models/ward_boy.py
@@ -4,3 +4,13 @@ from src.models.person import Person
 class WardBoy(Person):
     salary: int
     room_number: int
+
+    def __init__(self, person_id: int, first_name: str, last_name: str, phone: str,
+                 salary: int, room_number: int):
+        super().__init__(person_id, first_name, last_name, phone)
+        self.salary = salary
+        self.room_number = room_number
+
+    def display_info(self):
+        super().display_info()
+        print(f"Salary: {self.salary}, Room Number: {self.room_number}")
