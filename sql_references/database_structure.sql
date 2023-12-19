31adfb001665a18a68cf08aca6ebb255f7b24c34
Ahmed Hosny <ahmhosny2002@gmail.com>
2023-12-20 01:02:49 +0200

    Update database structure

--- sql_references/database_structure.sql
+++ sql_references/database_structure.sql
@@ -3,134 +3,140 @@ USE MediCareHub;
 
 -- Patients table
 CREATE TABLE IF NOT EXISTS Patient (
-    PatientID INT AUTO_INCREMENT PRIMARY KEY,
+    ID INT AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(30) NOT NULL,
     LastName VARCHAR(30) NOT NULL,
-    PhoneNumber VARCHAR(11) NOT NULL,
+    Phone VARCHAR(11) NOT NULL,
     Gender TINYINT NOT NULL,
-    DateOfBirth DATE,
+    DOB DATE,
     HomeAddress VARCHAR(255),
     ChronicDiseases VARCHAR(1023),
     Balance DECIMAL(10, 2) DEFAULT 0,
-    NumberOfRecords INT DEFAULT 0
+    NumberOfRecords INT DEFAULT 0,
+    INDEX phone(Phone)
 );
 
 -- Room Type table
 CREATE TABLE IF NOT EXISTS RoomType (
-    TypeID INT AUTO_INCREMENT PRIMARY KEY,
-    TypeDescription VARCHAR(30)
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    TypeDescription VARCHAR(30) NOT NULL,
+    INDEX description(TypeDescription)
 );
 
 -- Room table
 CREATE TABLE IF NOT EXISTS Room (
-    RoomID INT AUTO_INCREMENT PRIMARY KEY,
-    RoomNumber INT UNIQUE,
-    RoomCapacity INT,
-    RoomTypeID INT,
-    FOREIGN KEY (RoomTypeID) REFERENCES RoomType(TypeID)
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    RoomNumber INT UNIQUE NOT NULL,
+    TypeID INT NOT NULL,
+    FOREIGN KEY (TypeID) REFERENCES RoomType(ID),
+    INDEX Room(RoomNumber)
 );
 
 -- Doctor table
 CREATE TABLE IF NOT EXISTS Doctor (
-    DoctorID INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(30),
-    LastName VARCHAR(30),
-    PhoneNumber VARCHAR(11),
-    EmailAddress VARCHAR(320),
-    JobTitle VARCHAR(255),
-    OfficeNumber INT,
-    Salary DECIMAL(10, 2),
-    FOREIGN KEY (OfficeNumber) REFERENCES Room(RoomNumber)
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(30) NOT NULL,
+    LastName VARCHAR(30) NOT NULL,
+    Phone VARCHAR(11) NOT NULL,
+    Email VARCHAR(320) NOT NULL,
+    JobTitle VARCHAR(30) NOT NULL,
+    OfficeID INT NOT NULL,
+    Salary DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (OfficeID) REFERENCES Room(ID),
+    INDEX phone(Phone)
 );
 
 -- Nurse table
 CREATE TABLE IF NOT EXISTS Nurse (
-    NurseID INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(255),
-    LastName VARCHAR(255),
-    PhoneNumber VARCHAR(11),
-    EmailAddress VARCHAR(320),
-    RoomNumber INT,
-    Salary DECIMAL(10, 2),
-    FOREIGN KEY (RoomNumber) REFERENCES Room(RoomNumber)
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(30) NOT NULL,
+    LastName VARCHAR(30) NOT NULL,
+    Phone VARCHAR(11) NOT NULL,
+    Email VARCHAR(320) NOT NULL,
+    RoomID INT NOT NULL,
+    Salary DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (RoomID) REFERENCES Room(ID),
+    INDEX phone(Phone)
 );
 
 -- Ward Boys table
 CREATE TABLE IF NOT EXISTS WardBoy (
-    WardBoyID INT AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(255),
-    LastName VARCHAR(255),
-    PhoneNumber VARCHAR(11),
-    RoomNumber INT,
-    Salary DECIMAL(10, 2),
-    FOREIGN KEY (RoomNumber) REFERENCES Room(RoomNumber)
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(30) NOT NULL,
+    LastName VARCHAR(30) NOT NULL,
+    Phone VARCHAR(11) NOT NULL,
+    RoomID INT NOT NULL,
+    Salary DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (RoomID) REFERENCES Room(ID),
+    INDEX phone(Phone)
 );
 
 -- Disease Record table
-CREATE TABLE IF NOT EXISTS DiseaseRecord (
-    RecordID INT ,
+CREATE TABLE IF NOT EXISTS MedicalRecord (
     PatientID INT,
-    AssignedRoomNumber INT,
-    AssignedDoctorID INT,
-    DiseaseDescription VARCHAR(255),
-    PriorityLevel INT,
+    ID INT,
+    RoomID INT NOT NULL,
+    DoctorID INT NOT NULL,
     AdmissionDate DATE,
-    DischargeDate DATE,
+    DiseaseDescription VARCHAR(1023),
+    Priority INT,
+    TreatmentDescription VARCHAR(1023),
     TreatmentCost DECIMAL(10, 2),
-    Treatment VARCHAR(255),
+    DischargeDate DATE,
     Status VARCHAR(50),
-    FOREIGN KEY (PatientID) REFERENCES Patient(PatientID),
-    PRIMARY KEY (PatientID, RecordID),
-    FOREIGN KEY (AssignedRoomNumber) REFERENCES Room(RoomNumber),
-    FOREIGN KEY (AssignedDoctorID) REFERENCES Doctor(DoctorID)
+    FOREIGN KEY (PatientID) REFERENCES Patient(ID),
+    PRIMARY KEY (ID, PatientID),
+    FOREIGN KEY (RoomID) REFERENCES Room(ID),
+    FOREIGN KEY (DoctorID) REFERENCES Doctor(ID)
 );
 
 -- Bill table
 CREATE TABLE IF NOT EXISTS Bill (
-    BillID INT AUTO_INCREMENT PRIMARY KEY,
-    PatientID INT,
-    BillAmount DECIMAL(10, 2),
-    BillingDate DATE,
-    FOREIGN KEY (PatientID) REFERENCES Patient(PatientID)
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    PatientID INT NOT NULL,
+    BillAmount DECIMAL(10, 2) NOT NULL,
+    BillingDate DATETIME DEFAULT NOW(),
+    FOREIGN KEY (PatientID) REFERENCES Patient(ID),
+    INDEX patient(PatientID)
 );
 
 DELIMITER //
-CREATE TRIGGER IF NOT EXISTS BillInsertTrigger
+CREATE TRIGGER  BillInsertTrigger
 AFTER INSERT ON Bill FOR EACH ROW
 BEGIN
     UPDATE Patient
     SET Balance = Balance + NEW.BillAmount
-    WHERE PatientID = NEW.PatientID;
+    WHERE Patient.ID = NEW.PatientID;
 END;
 //
 DELIMITER ;
 
 DELIMITER //
-CREATE TRIGGER IF NOT EXISTS RecordInsertTrigger
-AFTER INSERT ON DiseaseRecord FOR EACH ROW
+CREATE TRIGGER  RecordInsertTrigger
+AFTER UPDATE ON MedicalRecord FOR EACH ROW
 BEGIN
     UPDATE Patient
-    SET Balance = Balance - NEW.TreatmentCost
-    WHERE PatientID = NEW.PatientID;
+    SET Balance = Balance - NEW.TreatmentCost + OLD.TreatmentCost
+    WHERE Patient.ID = NEW.PatientID;
 END;
 //
 DELIMITER ;
 
 
 DELIMITER //
-CREATE TRIGGER IF NOT EXISTS BeforeInsertDiseaseRecord
-BEFORE INSERT ON DiseaseRecord FOR EACH ROW
+CREATE TRIGGER  BeforeInsertDiseaseRecord
+BEFORE INSERT ON MedicalRecord FOR EACH ROW
 BEGIN
     DECLARE numRecords INT;
 
     -- Get the number of records for the patient
-    SELECT NumberOfRecords INTO numRecords FROM Patient WHERE PatientID = NEW.PatientID;
+    SELECT NumberOfRecords INTO numRecords FROM Patient WHERE Patient.ID = NEW.PatientID;
 
     -- Increment the count to get the new RecordID
-    SET NEW.RecordID = numRecords;
+    SET NEW.ID = numRecords + 1;
     UPDATE Patient
     SET NumberOfRecords = NumberOfRecords + 1
-    WHERE PatientID = NEW.PatientID;
+    WHERE Patient.ID = NEW.PatientID;
 END;
 //
 DELIMITER ;
